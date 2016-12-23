dd7c917d8bb5032429a75379d79e9b4b6aa7c614
Fang Ernesto <zxc9799979@gmail.com>
2016-12-23 22:14:51 +0800

    Q3 ActiveRecord Scope

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  scope :pending, -> { where( :status => "pending" ) }
+  scope :completed, -> { where( :status => "completed" ) }
+  scope :within_days, ->(x) { where("created_at > ?", x.days) }
 end
